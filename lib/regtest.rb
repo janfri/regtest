2100980a7afc4d1d3849f01e4111f3a1fd1c56d0
Jan Friedrich <janfri26@gmail.com>
2014-02-04 21:27:45 +0100

    Printing some statistics.

--- lib/regtest.rb
+++ lib/regtest.rb
@@ -9,7 +9,9 @@ require 'yaml'
 
 module Regtest
 
-  @@results = {}
+  @count = 0
+  @results = {}
+  @start = Time.now
 
   def sample name
     h = {}
@@ -21,17 +23,18 @@ module Regtest
       h['exception'] = e.message
     end
     output_filename = caller.first.split(/:/).first.sub(/\.rb/, '') << '.yml'
-    unless @@results[output_filename]
-      puts unless @@results.empty?
+    unless Regtest.results[output_filename]
+      puts unless Regtest.results.empty?
       puts File.basename(output_filename, '.yml')
-      @@results[output_filename] = []
+      Regtest.results[output_filename] = []
     end
-    @@results[output_filename] << h
+    Regtest.results[output_filename] << h
+    Regtest.count += 1
     print '.'; $stdout.flush
   end
 
-  def self.results
-    @@results
+  class << self
+    attr_accessor :count, :results, :start
   end
 
   module_function :sample
@@ -40,7 +43,10 @@ end
 
 at_exit do
   ARGV.each {|a| load a}
-  puts
+  sample_count = Regtest.count
+  sample_time = Time.now - Regtest.start
+  puts format("\n\n%d samples executed in %.2f s (%.2f samples/s)", sample_count, sample_time, sample_count / sample_time)
+  save_start = Time.now
   Regtest.results.each_pair do |filename,arr|
     File.open(filename, 'w') do |f|
       arr.each do |h|
@@ -48,4 +54,7 @@ at_exit do
       end
     end
   end
+  files_count = Regtest.results.size
+  save_time = Time.now - save_start
+  puts format("%d files written in %.2f s (%.2f files/s)", files_count, save_time, files_count / save_time)
 end
