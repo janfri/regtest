3307e10350788a5fad032cdb6970e52474ff686d
Jan Friedrich <janfri26@gmail.com>
2016-01-06 12:30:19 +0100

    Bugfix REGTEST_FILES: Distinguish between rb and yml files.

--- lib/regtest/task.rb
+++ lib/regtest/task.rb
@@ -1,6 +1,8 @@
-REGTEST_FILES = FileList.new('regtest/**/*.{rb,yml}')
+REGTEST_FILES_RB = FileList.new('regtest/**/*.rb')
+REGTEST_FILES_YML = FileList.new('regtest/**/*.yml')
+REGTEST_FILES = REGTEST_FILES_RB + REGTEST_FILES_YML
 
 desc 'Run regression tests'
 task :regtest do
-  sh "ruby -I lib:regtest #{REGTEST_FILES}"
+  sh "ruby -I lib:regtest #{REGTEST_FILES_RB}"
 end
