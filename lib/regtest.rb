439b879bb5bf6deb5b16ecb62fcebdfa8e2f212a
Jan Friedrich <janfri26@gmail.com>
2017-08-18 17:08:24 +0200

    Add encoding and frozen_string_literal pragma.

--- lib/regtest.rb
+++ lib/regtest.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+# frozen_string_literal: true
 #
 # Regtest - Simple Regression Testing For Ruby Projects
 #
--- lib/regtest/task.rb
+++ lib/regtest/task.rb
@@ -1,3 +1,6 @@
+# encoding: utf-8
+# frozen_string_literal: true
+
 REGTEST_FILES_RB = FileList.new('regtest/**/*.rb')
 REGTEST_FILES_YML = FileList.new('regtest/**/*.yml')
 REGTEST_FILES = REGTEST_FILES_RB + REGTEST_FILES_YML
