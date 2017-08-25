f103f0147ea457b66b11838d95b06f3e4aa0df41
Jan Friedrich <janfri26@gmail.com>
2017-08-25 21:11:15 +0200

    Tweak metatest.

--- regtest/metatest.rb
+++ regtest/metatest.rb
@@ -9,16 +9,18 @@ require 'tmpdir'
 Dir.mktmpdir('regtest') do |tmpdir|
   Dir.chdir __dir__ do
     Dir['*.rb'].each do |filename|
+      # beware of endless recursion
       next if filename =~ /metatest/
       FileUtils.cp filename, tmpdir
     end
-    Dir.chdir tmpdir do
-      _, o, e = Open3.popen3("ruby -I #{File.join(__dir__, '../lib')} #{Dir['*.rb'].sort.join(' ')}")
-      Regtest.sample 'metatest' do
-        res = {'stdout' => o.read, 'stderr' => e.read}
-        res['stdout'].gsub!(/\d+\.\d+/, 'x.xx')
-        res
-      end
+  end
+  Dir.chdir tmpdir do
+    _, o, e = Open3.popen3("ruby -I #{File.join(__dir__, '../lib')} #{Dir['*.rb'].sort.join(' ')}")
+    Regtest.sample 'metatest' do
+      res = {'stdout' => o.read, 'stderr' => e.read}
+      # levelling runtime specific values
+      res['stdout'].gsub!(/\d+\.\d+/, 'x.xx')
+      res
     end
   end
 end
