9731d7292698dd64089b8f2019757eead23fd612
Jan Friedrich <janfri26@gmail.com>
2017-08-31 20:39:02 +0200

    Add plugin regtest/colorize.

---
+++ lib/regtest/colorize.rb
@@ -0,0 +1,36 @@
+# encoding: utf-8
+# frozen_string_literal: true
+
+begin
+  require 'colorized_string'
+
+  module Regtest
+
+    module Colorize
+
+      # Color mapping (you can change it via Regtest.colors)
+      @colors = (%i(success fail unknown_result filename statistics).zip %i(green red yellow cyan default)).to_h
+
+      def report *args, type: nil
+        color = Colorize.colors[type]
+        if color && $stdout.tty?
+          args = args.map {|a| ColorizedString[a.to_s].colorize(color)}
+        end
+        puts *args
+      end
+
+      class << self
+        attr_reader :colors
+      end
+
+    end
+
+    class << self
+      prepend Colorize
+    end
+
+  end
+rescue LoadError
+  warn 'gem colorize not found'
+  warn 'plugin regtest/colorize disabled'
+end
