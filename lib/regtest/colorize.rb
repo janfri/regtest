b8a907bd4a70fee5c86eedf6bea034acea305016
Jan Friedrich <janfri26@gmail.com>
2017-09-06 14:45:50 +0200

    Define Regtest::Colorize.mapping accessors even if colorize gem is notc available.

--- lib/regtest/colorize.rb
+++ lib/regtest/colorize.rb
@@ -1,17 +1,26 @@
 # encoding: utf-8
 # frozen_string_literal: true
 
-begin
-  require 'colorized_string'
+module Regtest
 
-  module Regtest
+  # Regtest plugin to have colorized reports.
+  # It uses the great colorized gem
+  # (see https://rubygems.org/gems/colorize)
+  module Colorize
 
-    # Regtest plugin to have colorized reports.
-    # It uses the great colorized gem
-    # (see https://rubygems.org/gems/colorize)
-    module Colorize
+    @mapping = (%i(success fail unknown_result filename statistics).zip %i(green red yellow cyan default)).to_h
 
-      @mapping = (%i(success fail unknown_result filename statistics).zip %i(green red yellow cyan default)).to_h
+    class << self
+      # Color mapping (all parameters of ColorizedString.colorize are supported
+      # (see https://github.com/fazibear/colorize#usage)
+      # Examples:
+      #   Regtest::Colorize.mapping[:filename] = :light_blue
+      #   Regtest::Colorize.mapping[:statistics] = {color: :cyan, mode: :italic}
+      attr_accessor :mapping
+    end
+
+    begin
+      require 'colorized_string'
 
       # Redefine Regtest.report.
       def report *args, type: nil
@@ -21,24 +30,15 @@ begin
         end
         puts *args
       end
-
-      class << self
-        # Color mapping (all parameters of ColorizedString.colorize are supported
-        # (see https://github.com/fazibear/colorize#usage)
-        # Examples:
-        #   Regtest::Colorize.mapping[:filename] = :light_blue
-        #   Regtest::Colorize.mapping[:statistics] = {color: :cyan, mode: :italic}
-        attr_accessor :mapping
-      end
-
-    end
-
-    class << self
-      prepend Colorize
+    rescue LoadError
+      warn 'gem colorize not found'
+      warn 'plugin regtest/colorize disabled'
     end
 
   end
-rescue LoadError
-  warn 'gem colorize not found'
-  warn 'plugin regtest/colorize disabled'
+
+  class << self
+    prepend Colorize
+  end
+
 end
