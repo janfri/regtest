93ec4efd0cac5f6c995b90d9508022b937e190c7
Jan Friedrich <janfri26@gmail.com>
2019-03-04 21:48:28 +0100

    Remove superfluous Copyright notice

--- lib/regtest/task.rb
+++ lib/regtest/task.rb
@@ -1,9 +1,5 @@
 # encoding: utf-8
 # frozen_string_literal: true
-#
-# Copyright 2014, 2016, 2017 by Jan Friedrich <janfri26@gmail.com>
-# License: Regtest is licensed under the same terms as Ruby itself.
-#
 
 # Regtest sample files (= Ruby files)
 REGTEST_FILES_RB = FileList.new('regtest/**/*.rb')
