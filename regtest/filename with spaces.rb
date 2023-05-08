18be47dd30d4f27da729a0911df2ed4eaf73bfa6
Jan Friedrich <janfri26@gmail.com>
2023-05-08 20:38:43 +0200

    Fix typo

--- regtest/filename with spaces.rb
+++ regtest/filename with spaces.rb
@@ -4,5 +4,5 @@
 require 'regtest'
 
 Regtest.sample 'spaces in filenames' do
-  "If you can read this in the results file #{File.basename(__FILE__.sub(/\.rb/, '.yml')).inspect} than regtest can handle filenames with spaces. :)"
+  "If you can read this in the results file #{File.basename(__FILE__.sub(/\.rb/, '.yml')).inspect} then regtest can handle filenames with spaces. :)"
 end
