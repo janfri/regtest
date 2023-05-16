022fcc6b28afca878c37d50844b8061ef08108e2
Jan Friedrich <janfri26@gmail.com>
2023-05-16 20:49:11 +0200

    Remove colorize

--- lib/regtest/colorize.rb
+++
@@ -1,8 +0,0 @@
-# encoding: utf-8
-# frozen_string_literal: true
-
-require 'regtest/colors'
-
-Regtest::Colorize = Regtest::Colors
-
-warn 'regtest/colorize is deprecated. use regtest/colors instead.'
