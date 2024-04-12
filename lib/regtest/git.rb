94b18a71fd589241dc9976af0ebc6b3878e13779
Jan Friedrich <janfri26@gmail.com>
2024-04-12 20:37:11 +0200

    Implement Regtest::Git.work_tree and Regtest::Git.git_dir

--- lib/regtest/git.rb
+++ lib/regtest/git.rb
@@ -3,54 +3,75 @@
 
 require 'open3'
 
-# Check if git is available and current directory is part of a git repository.
+# Check if git is available
 begin
-  _, ps = Open3.capture2e('git status --porcelain')
-  if ps.exitstatus == 0
-    module Regtest
+  Open3.capture2e('git --version')
+  module Regtest
 
-      # Regtest plugin for git (results are checked automatically when running
-      # regtest)
-      module Git
+    # Regtest plugin for git (results are checked automatically when running
+    # regtest)
+    module Git
 
-        # Redefine Regtest.check_results.
-        def check_results
-          output_files = Regtest.results.keys
-          if output_files.empty?
-            report "\nNothing to do.", type: :success
-            return :success
-          end
-          git_stat, _, _ = Open3.capture3(*%w(git status --porcelain --), *output_files)
-          case git_stat
-          when /^.M/ # at least one modified file
-            report "\nThere are changes in your sample results!", type: :fail
-            system *%w(git status -s --), *output_files
-            return :fail
-          when /^.\?/ # at least one unknown file
-            report "\nThere is at least one new sample result file.", type: :unknown_result
-            system *%w(git status -s --), *output_files
-            return :unknown_result
-          when '', /^. / # no changes in (maybe staged) files
-            report "\nLooks good. :)", type: :success
-            system *%w(git status -s --), *output_files
-            return :success
-          else
-            report "\nYour sample results are in a bad condition!", type: :fail
-            system *%w(git status -s --), *output_files
-            return :fail
-          end
+      # Redefine Regtest.check_results.
+      def check_results
+        output_files = Regtest.results.keys
+        if output_files.empty?
+          report "\nNothing to do.", type: :success
+          return :success
         end
-
+        git_status = ['git', git_global_args, %w(status -s --)].flatten
+        git_results, stderr, _ = Open3.capture3(*git_status, *output_files)
+        unless stderr.empty?
+          report "\ngit command coud not be executed!", type: :fail
+          return :fail
+        end
+        case git_results
+        when /^.M/ # at least one modified file
+          report "\nThere are changes in your sample results!", type: :fail
+          system *git_status, *output_files
+          return :fail
+        when /^.\?/ # at least one unknown file
+          report "\nThere is at least one new sample result file.", type: :unknown_result
+          system *git_status, *output_files
+          return :unknown_result
+        when '', /^. / # no changes in (maybe staged) files
+          report "\nLooks good. :)", type: :success
+          system *git_status, *output_files
+          return :success
+        else
+          report "\nYour sample results are in a bad condition!", type: :fail
+          system *git_status, *output_files
+          return :fail
+        end
       end
 
       class << self
-        prepend Git
+        # git parameter +--work-tree+
+        attr_accessor :work_tree
+
+        # git parameter +--git-dir+
+        attr_accessor :git_dir
+      end
+
+      private
+
+      def git_global_args
+        args = []
+        if wt = Regtest::Git.work_tree
+          args << format('--work-tree=%s', wt)
+        end
+        if gt = Regtest::Git.git_dir
+          args << format('--git-dir=%s', gt)
+        end
+        args
       end
 
     end
-  else
-    warn 'current directory is not part of a git repository:'
-    warn 'plugin regtest/git disabled'
+
+    class << self
+      prepend Git
+    end
+
   end
 rescue Errno::ENOENT
   warn 'git command not found: plugin regtest/git disabled'
---
+++ regtest/metatest_git_options.rb
@@ -0,0 +1,60 @@
+# encoding: utf-8
+# frozen_string_literal: true
+
+# If git isn't available skip this file
+begin
+  require 'open3'
+  Open3.capture2e('git --version')
+
+  require 'fileutils'
+  require 'regtest'
+  require 'regtest/git'
+  require 'tmpdir'
+
+  $my_dir = File.expand_path(__dir__)
+
+  def create_sample name
+    lib_dir = File.join($my_dir, '../lib')
+    o, e, ps = Open3.capture3(*[{'NOREGTESTRC' => 'true'}, 'ruby', '-I', lib_dir, '-r', 'regtest', '-r', 'regtest/git'], *Dir['work_tree/*.rb'].sort)
+    Regtest.sample name do
+      res = {'stdout' => o, 'stderr' => e}
+      # Levelling out runtime specific differences.
+      res['stdout'].gsub!(/\d+\.\d+/, 'x.xx')
+      res['stdout'].gsub!(%r(\d+ samples/s), 'x samples/s')
+      res['stdout'].gsub!(/^\?\? filename with spaces.yml$/m, '?? "filename with spaces.yml"')
+      res['exitstatus'] = ps.exitstatus
+      res
+    end
+  end
+
+  def execute cmd
+    Open3.capture2e cmd
+  end
+
+  SAMPLE = <<-END
+  Regtest.sample 'sample' do
+    'sample result'
+  end
+  END
+
+  Dir.mktmpdir('regtest') do |tmpdir|
+    Dir.chdir tmpdir do
+      work_tree_dir = File.join(tmpdir, 'work_tree')
+      git_dir = File.join(work_tree_dir, '.git')
+      FileUtils.mkdir work_tree_dir
+      Dir.chdir work_tree_dir do
+        File.write('sample.rb', SAMPLE)
+        execute 'git init'
+        execute 'git add sample.rb'
+      end
+      create_sample 'without --work-tree and without --git-dir'
+      File.write File.join(work_tree_dir, 'init.rb'), "Regtest::Git.work_tree = '#{work_tree_dir}'"
+      create_sample 'with --work-tree and without --git-dir'
+      File.write File.join(work_tree_dir, 'init.rb'), "Regtest::Git.git_dir = '#{git_dir}'"
+      create_sample 'without --work-tree and with --git-dir'
+      File.write File.join(work_tree_dir, 'init.rb'), "Regtest::Git.work_tree = '#{work_tree_dir}'; Regtest::Git.git_dir = '#{git_dir}'"
+      create_sample 'with --work-tree and with --git-dir'
+    end
+  end
+rescue Errno::ENOENT
+end
